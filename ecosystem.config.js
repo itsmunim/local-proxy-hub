e19458e32417369fc0854d81c9056fb7774b3626
dibosh <dibosh@newscred.com>
2018-09-01 15:33:09 +0600

    None of the watch config is working, lets try this

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,15 @@ module.exports = {
   apps: [{
     name: 'lph-server',
     script: './lib/server.js',
-    watch: './lib/routes.json',
-    ignore_watch: []
+    watch: true,
+    ignore_watch: [
+      'node_modules',
+      '.editorconfig',
+      '.gitignore',
+      'package.json',
+      'ecosystem.config.js',
+      'yarn.lock',
+      '.idea'
+    ]
   }]
 };
