fed917d46decbd335b8dd47bac00888eaab8eb92
dibosh <dibosh@newscred.com>
2018-09-01 14:56:41 +0600

    Get rid of watch options since they make actual watching slow

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,6 @@ module.exports = {
   apps: [{
     name: 'lph-server',
     script: './lib/server.js',
-    watch: './lib/routes.json',
-    watch_options: {
-      usePolling: true,
-      alwaysStat: true,
-      useFsEvents: false
-    }
+    watch: './lib/routes.json'
   }]
 };
